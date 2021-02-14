7297459331019a68b4befd02b72e9f848c3ee75a
Ariful Islalm <arifsujon.bd@gmail.com>
2021-02-14 21:32:04 +0600

    add display error message

--- app.js
+++ app.js
@@ -15,94 +15,99 @@ const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
 // show images 
 const showImages = (images) => {
-    imagesArea.style.display = 'block';
-    gallery.innerHTML = '';
-    // show gallery title
-    galleryHeader.style.display = 'flex';
-    images.forEach(image => {
-      let div = document.createElement('div');
-      div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-      div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
-      gallery.appendChild(div)
-    })
-    toggleSpinner();
+  imagesArea.style.display = 'block';
+  gallery.innerHTML = '';
+  // show gallery title
+  galleryHeader.style.display = 'flex';
+  images.forEach(image => {
+    let div = document.createElement('div');
+    div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
+    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+    gallery.appendChild(div)
+  })
+  toggleSpinner();
 }
 
-// Key-Board Enter Button Functionality for Search Button
+// Enter key Functionality for Search Button
 searchField = document.getElementById("search")
-    .addEventListener("keypress", function(event) {
-        if (event.key == 'Enter') {
-            document.getElementById("search-btn").click();
-        }
-    });
+  .addEventListener("keypress", function (event) {
+    if (event.key == 'Enter') {
+      document.getElementById("search-btn").click();
+    }
+  });
 
-// Toggle Spinner
+// Toggle Spinner for search image
 const toggleSpinner = () => {
-    const spinner = document.getElementById('loading-spinner');
-    spinner.classList.toggle('d-none');
-    
+  const spinner = document.getElementById('loading-spinner');
+  spinner.classList.toggle('d-none');
 }
 
 const getImages = (query) => {
-    toggleSpinner(true);
-    fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
-      .then(response => response.json())
-      .then(data => showImages(data.hits))
-      .catch(err => console.log(err))   
+  toggleSpinner(true);
+  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
+    .then(response => response.json())
+    .then(data => showImages(data.hits))
+    .catch(error => displayError('Something went wrong! Please try again later.'));
+}
+
+// display error message
+const displayError = error => {
+  const errorTag = document.getElementById('error-message');
+  errorTag.innerHTML = error;
 }
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
-    let element = event.target;
-    element.classList.toggle('added');
-    
-    let item = sliders.indexOf(img);
-    console.log(item);
-    if (item === -1) {
-      sliders.push(img);
-      
-    } 
-    if (item > -1) {
-        sliders.splice(item, 1);
-    }
+  let element = event.target;
+  element.classList.toggle('added');
+
+  let item = sliders.indexOf(img);
+  console.log(item);
+  if (item === -1) {
+    sliders.push(img);
+
+  }
+  if (item > -1) {
+    sliders.splice(item, 1);
+  }
 }
 var timer
 const createSlider = () => {
-    // check slider image length
-    if (sliders.length < 2) {
-      alert('Select at least 2 image.')
-      return;
-    }
-    // crate slider previous next area
-    sliderContainer.innerHTML = '';
-    const prevNext = document.createElement('div');
-    prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
-    prevNext.innerHTML = ` 
+  // check slider image length
+  if (sliders.length < 2) {
+    alert('Select at least 2 image.')
+    return;
+  }
+  // crate slider previous next area
+  sliderContainer.innerHTML = '';
+  const prevNext = document.createElement('div');
+  prevNext.className = "prev-next d-flex w-100 justify-content-between align-items-center";
+  prevNext.innerHTML = ` 
     <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
     <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
     `;
-
-    sliderContainer.appendChild(prevNext)
-    document.querySelector('.main').style.display = 'block';
-    // hide image aria
-    imagesArea.style.display = 'none';
-    let duration = document.getElementById('duration-field').value || 1000;
-        if(duration < 0){
-            duration = duration * (-1);
-        }
-    sliders.forEach(slide => {
-        let item = document.createElement('div')
-        item.className = "slider-item";
-        item.innerHTML = `<img class="w-100"
+  
+  sliderContainer.appendChild(prevNext)
+  document.querySelector('.main').style.display = 'block';
+  // hide image aria
+  imagesArea.style.display = 'none';
+  let duration = document.getElementById('duration-field').value || 1000;
+  if (duration < 0) {
+    duration = duration * (-1);
+  }
+  sliders.forEach(slide => {
+    let item = document.createElement('div')
+    item.className = "slider-item";
+    item.innerHTML = `<img class="w-100"
         src="${slide}"
         alt="">`;
-        sliderContainer.appendChild(item)
-    })
-    changeSlide(0)
-    timer = setInterval(function () {
-      slideIndex++;
-      changeSlide(slideIndex);
-    }, duration);
+    sliderContainer.appendChild(item)
+  })
+  changeSlide(0)
+  timer = setInterval(function () {
+    slideIndex++;
+    changeSlide(slideIndex);
+  }, duration);
 }
 
 // change slider index 
@@ -113,32 +118,40 @@ const changeItem = index => {
 // change slide item
 const changeSlide = (index) => {
 
-    const items = document.querySelectorAll('.slider-item');
-    if (index < 0) {
-      slideIndex = items.length - 1
-      index = slideIndex;
-    };
+  const items = document.querySelectorAll('.slider-item');
+  if (index < 0) {
+    slideIndex = items.length - 1
+    index = slideIndex;
+  };
 
-    if (index >= items.length) {
-      index = 0;
-      slideIndex = 0;
-    }
+  if (index >= items.length) {
+    index = 0;
+    slideIndex = 0;
+  }
 
-    items.forEach(item => {
-      item.style.display = "none"
-    })
+  items.forEach(item => {
+    item.style.display = "none"
+  })
 
-    items[index].style.display = "block"
+  items[index].style.display = "block"
 }
 
 searchBtn.addEventListener('click', function () {
-    document.querySelector('.main').style.display = 'none';
-    clearInterval(timer);
-    const search = document.getElementById('search');
-    getImages(search.value)
-    sliders.length = 0;
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  const search = document.getElementById('search');
+  getImages(search.value)
+  sliders.length = 0;
 })
 
 sliderBtn.addEventListener('click', function () {
-    createSlider()
+  createSlider();
 })
+
+// enter key: when user fill duration and Immediately press enter key then it will work
+durationSearchField = document.getElementById("duration-field")
+  .addEventListener("keypress", function (event) {
+    if (event.key == 'Enter') {
+      document.getElementById("create-slider").click();
+    }
+  });
