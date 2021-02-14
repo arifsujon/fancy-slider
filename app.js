f86d6bcd7794c3f14a3a00ed6b9ea19c9519d834
Ariful Islalm <arifsujon.bd@gmail.com>
2021-02-14 08:35:26 +0600

    deselect bug fix

--- app.js
+++ app.js
@@ -54,13 +54,16 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
     let element = event.target;
-    element.classList.add('added');
-  
+    element.classList.toggle('added');
+    
     let item = sliders.indexOf(img);
+    console.log(item);
     if (item === -1) {
       sliders.push(img);
-    } else {
-      alert('Hey, Already added !')
+      
+    } 
+    if (item > -1) {
+        sliders.splice(item, 1);
     }
 }
 var timer
